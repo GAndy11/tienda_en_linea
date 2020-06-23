be74e2aece97149b4ae26ee7876c30c050cdf833
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2020-06-22 19:30:44 -0500

    Primeras páginas en español de los formularios de usuario

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Ya estás logueado!
-                </div>
-            </div>
-        </div>
+        <h1>Bienvenido a la Tienda en Línea de Evertec</h1>
+        
     </div>
 </div>
 @endsection
