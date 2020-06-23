424ccfaa884e95579e21a9d7a08192cf171fc92a
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2020-06-22 20:41:19 -0500

    Interactuando con el home de la ṕágina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <h1>Bienvenido a la Tienda en Línea de Evertec</h1>
-        
     </div>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus at pariatur impedit minus laudantium repudiandae recusandae aliquam neque. Maiores laboriosam culpa sit deserunt magnam quod, quidem aperiam excepturi optio eos.</p>
+    <br>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure laudantium distinctio, ipsam adipisci incidunt consequuntur aspernatur nulla quas sed at suscipit dolore doloribus molestiae eos blanditiis odio sunt veritatis provident?</p>
 </div>
 @endsection
